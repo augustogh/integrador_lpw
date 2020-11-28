7867fdf6949417b7e025a63ad49770d9eced67ef
Augusto Lobelos <augusto.lobelos@gmail.com>
2020-11-28 13:41:04 -0300

    cambio en index y actualización en style

--- www/index.php
+++ www/index.php
@@ -1,37 +1,25 @@
 <html>
-	<body>
-		<title>TP2</title>
-		<div>
-			<h3>Datos Personales</h3>
+	<head>
+		<link rel="stylesheet" href="./css/style.css">
+	</head>
+	<body >
+	<header>
+        <section class="wrapper">
+			<nav>
+				<ul>
+					<li><a href="#inicio">Home</a></li>
+					<li><a href="#novedades">News</a></li>
+					<li><a href="#contacto">Contact</a></li>
+					<li style="float:right"><a class="active" href="#ingreso">About</a></li>
+				</ul>
+			</nav>
+		</section>
+	</header>
+	
+		<div id="cf">
+			<img class="bottom" src="./img/img03.jpg" />
+			<img class="top" src='./img/img04.jpg' />
 		</div>
-		<form action="dpersonal.php" method="POST" enctype="multipart/form-data">
-			<table>
-				<tbody>
-					<tr>
-						<td>&nbsp;Nombre:</td>
-						<td>&nbsp;<input type="nombre" name="a_nombre" value=""/> <br/></td>
-					</tr>
-					<tr>
-						<td>&nbsp;Apellido:</td>
-						<td>&nbsp;<input type="apellido" name="b_apellido" value=""/><br/></td>
-					</tr>
-					<tr>
-						<td>&nbsp;DNI:</td>
-						<td>&nbsp;<input type="dni" name="b_dni" value=""/><br/></td>
-					</tr>
-					<tr>
-						<td>&nbsp;Fecha de Nacimiento: </td>
-						<td>&nbsp;<input type="dni" name="b_dni" value=""/><br/></td>
-					</tr>
-				</tbody>
-			</table>
-			<br/>
-			<div>
-				<label for="file" name="file" id="file" /> <label for="file">Archivo:</label>
-				<br/> 
-				<input type="file" name="file" id="file" />
-				<br/>
-				<input type="submit" name="submit" id="Submit" />
-			</div>
+
 	</body>
-</html>
+</html> 
