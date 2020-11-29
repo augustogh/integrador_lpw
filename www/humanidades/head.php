5b07ed9f0c3353059d72bbde53f2b1f9b8b57036
Augusto Lobelos <augusto.lobelos@gmail.com>
2020-11-29 17:57:32 -0300

    look & feel . usuario y contraseña

---
+++ www/humanidades/head.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <div class="coco">
+        <!---<img class="arrow" src="assets/images/demo-arrow-up.png" alt="arrow" height="120">-->
+        <h1>Facultad de medicina</h1>
+        <ul>
+            <li><a href="index.html" class="active">Basic</a></li>
+            <li><a href="navigation-icons.html">Icons</a></li>
+            <li><a href="navigation-dark.html">Icons Dark</a></li>
+            <li><a href="navigation-round.html">Round</a></li>
+        </ul>
+    </div>
+</body>
+</html>
--- www/index.php
+++ www/index.php
@@ -1,25 +1,63 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="./css/style.css">
+		<link rel="stylesheet" href= 
+"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+      
+    <script src= 
+"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"> 
+    </script> 
 	</head>
 	<body >
 	<header>
         <section class="wrapper">
 			<nav>
 				<ul>
-					<li><a href="#inicio">Home</a></li>
-					<li><a href="#novedades">News</a></li>
-					<li><a href="#contacto">Contact</a></li>
-					<li style="float:right"><a class="active" href="#ingreso">About</a></li>
+					<li><a href="#home">inicio</a></li>
+					<li><a href="#news">novedades</a></li>
+					<li><a href="#contact">contacto</a></li>
+					<li style="float:right"><a class="active" href="#login" onClick="SW_ACC()">ingreso</a></li>
 				</ul>
 			</nav>
 		</section>
 	</header>
 	
+    <script> 
+        function show(divId) { 
+            $("#" + divId).show(); 
+        } 
+  
+        function SW_ACC() { 
+            show('login');
+        } 
+    </script> 
+    </script> 
+<!--
 		<div id="cf">
 			<img class="bottom" src="./img/img03.jpg" />
 			<img class="top" src='./img/img04.jpg' />
 		</div>
+	-->
+
+
+	<nav class="contacc" >
+		<div class="acceso" id="login">
+		<form action="head.php" method="POST" enctype="multipart/form-data">
+				<table>
+					<tbody>
+						<tr>
+							<td>&nbsp;Usuario:</td>
+							<td>&nbsp;<input type="nombre" name="a_nombre" value=""/> <br/></td>
+						</tr>
+						<tr>
+							<td>&nbsp;Contraseña:</td>
+							<td>&nbsp;<input type="apellido" name="b_apellido" value=""/><br/></td>
+						</tr>
+
+					</tbody>
+				</table>
+		</div>
+	</nav>
 
 	</body>
 </html> 
